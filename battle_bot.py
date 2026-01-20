cea52266abb983c4299dc0ad7f78102f2736f7ba
Frank Morton <fmorton@mac.com>
2026-01-20 12:18:41 -0500

    add battle_bot

--- battle_bot.py
+++ battle_bot.py
@@ -1,11 +1,11 @@
 from robot.tasks import Tasks
 from robot.hummingbird_dual_motor_driver import HummingbirdDualMotorDriver
+from robot.xbox_joystick import XboxJoystick
 
 from battle_bot_driving import driving
 from battle_bot_event_manager import event_manager
 from battle_bot_extras import extras
 from battle_bot_weapons import weapons
-from xbox_joystick import XboxJoystick
 
 import asyncio
 
--- battle_bot_driving.py
+++ battle_bot_driving.py
@@ -1,4 +1,5 @@
 import asyncio
+import math
 import pygame
 
 from robot.tasks import Tasks
@@ -13,17 +14,48 @@ def driving_joystick_moved(hummingbird, instance_id, axis, value, joystick_minim
     if abs(value) < joystick_minimum:
         return
 
-    print("Joystick Axis:", axis, " Value:", value)
+    speed = -value * 100
 
-    # hummingbird.move(50, 50)
-    # sleep(0.1)
-    # hummingbird.move(0, 0)
+    print("Joystick Axis Driving:", axis, " Value:", value, "Speed:", speed)
+
+    if axis == 1:
+        if abs(speed) < 50:
+            speed = 0
+
+        print("DEBUG: DRIVING================================================================================================",speed)
+        #hummingbird.move(speed, speed)
+        #sleep(0.25)
+        #hummingbird.move(0, 0)
 
 
-async def driving(driving_queue, hummingbird, joystick, joystick_minimum=0.25):
+async def driving(driving_queue, hummingbird, joystick, joystick_minimum=0.40):
     running = True
 
     while running:
+        left_x_axis = joystick.joystick.get_axis(0)
+        left_y_axis = joystick.joystick.get_axis(1)
+
+        if abs(left_x_axis) < joystick_minimum:
+            left_x_axis = 0.0
+        if abs(left_y_axis) < joystick_minimum:
+            left_y_axis = 0.0
+
+        speed = math.sqrt(left_x_axis**2 + left_y_axis**2)
+
+        if left_y_axis != 0.0:
+            print("left_y_axis: ",left_y_axis)
+
+        if speed > 1.0:
+            speed = 1.0
+
+        angle_rad = math.atan2(-left_y_axis, left_x_axis)
+        angle_deg = math.degrees(angle_rad)
+        # if angle_deg < 0:
+        #    angle_deg += 360
+        #print(
+        #    "Axis:", left_x_axis, left_y_axis, "  speed:", speed, "  angle", angle_deg
+        #)
+
         try:
             event = driving_queue.get_nowait()
 
